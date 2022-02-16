27005db416d58529ef6e38498577d3e914461854
Johannes van Woerkom <johannesvw3@gmail.com>
2022-02-16 12:27:39 -0500

    Added pop up video and Contact Form

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,38 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//video pop-up
+
+window.document.onkeydown = function(e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+}
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  window.scrollTo(0, 0);
+  document.getElementById('light').style.display = 'block';
+  document.getElementById('fade').style.display = 'block';
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById('light').style.display = 'none';
+  document.getElementById('fade').style.display = 'none';
+  lightBoxVideo.pause();
+}
+
+document.querySelector('#contact-me').addEventListener("click", function() {
+  document.querySelector(".popup").classList.add('active');
+});
+
+document.querySelector('.popup .close-btn').addEventListener("click", function() {
+  document.querySelector(".popup").classList.remove('active');
+});
+
